031da9c8f0aaa38a108a684f2734e87c355b9749
y.watanabe <oomori.gohan39@gmail.com>
2025-01-26 21:56:17 +0900

    レビュー御願いします

---
+++ src/main/java/org/homework20250126/User.kt
@@ -0,0 +1,32 @@
+package org.homework20250126
+
+class User(
+        var username: String?,
+        val age: String?,
+        val password: String?,
+        val registrationDateTime: String?,
+        val userId: String?
+) {
+
+
+//    fun getUsername(): String? {
+//        return username
+//    }
+//
+//    private fun getAge(): String? {
+//        return age
+//    }
+//
+//    private fun getPassword(): String? {
+//        return password
+//    }
+//    private fun getRegistrationDateTime(): String? {
+//        return registrationDateTime
+//    }
+//    private fun getUserId(): String? {
+//        return userId
+//    }
+
+
+}
+
